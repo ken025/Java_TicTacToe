9085414e45cfbda0f69b7481ce0f5ec0dc66d5e5
ken025 <kennethmanuel250@gmail.com>
2021-03-24 23:46:06 -0400

    Prints X on board given user input

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
                 {'-', '+', '-', '+', '-'},
@@ -8,12 +10,53 @@ public class TicTacToe {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}
         };
+        printGameBoard(gameBoard);
 
-        for(char[] row : gameBoard){
-            for (char c : row){
-                System.out.print(c);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter placement: (1-9) ");
+        int pos = scan.nextInt();
+
+        System.out.println(pos);
+
+        switch (pos){
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
+
+        printGameBoard(gameBoard);
+    }
+
+        public static void printGameBoard(char[][] gameBoard ){
+            for (char[] row : gameBoard) {
+                for (char c : row) {
+                    System.out.print(c);
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
-}
